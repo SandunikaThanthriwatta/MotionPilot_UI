66b79cc79716dbff9fa79e07dbb2a18af8c1389c
Chiara-Vanderlan <115263438+Chiara-Vanderlan@users.noreply.github.com>
2024-07-14 23:22:20 +0530

    Completed UI development

--- main.py
+++ main.py
@@ -1,55 +1,53 @@
-# Import GUI file
-
 import os
 import sys
 
-from PyQt6.QtWidgets import QMainWindow, QApplication,QPushButton
-
+from PyQt6.QtCore import Qt, QPoint
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QButtonGroup, QSizeGrip
+from PyQt6.QtGui import QIcon
 
 import resources_rc
 from User_Interface import *
-# IMPORT Custom widgets
 
-########################################################################
-
-
-class MainWindow(QMainWindow,Ui_MainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__()
-        # QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        # Set the window icon
+        self.setWindowIcon(QIcon('SystemTray\logo.png'))
 
         self.switch_to_homePage()
         self.initialize_ui()
 
+        # Hide the title bar
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        # Make the window draggable
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.dragPosition = None
+
         self.ui.Restore_Button.clicked.connect(self.restore_or_maximize_window)
         self.ui.Home_Button.setChecked(True)
 
-        # self.ui.verticalSpacer_7.setHidden(True)
         self.ui.Library_Button_Container.setHidden(True)
         self.ui.Manual_Button_Container.setHidden(True)
         self.ui.More_Menu_Widget.setHidden(True)
-
-
 
         self.ui.Library_Button.clicked.connect(self.show_Library_Button_Container)
         self.ui.Manual_Button.clicked.connect(self.show_Manual_Button_Container)
         self.ui.Information_Button.clicked.connect(self.show_center_menu)
         self.ui.Settings_Button.clicked.connect(self.show_center_menu)
-
         self.ui.More_Menu_close_Button.clicked.connect(self.More_Menu_Remover)
 
         self.ui.Home_Button.clicked.connect(self.switch_to_homePage)
         self.ui.Manual_Button.clicked.connect(self.switch_to_ManualPage)
         self.ui.Library_Button.clicked.connect(self.switch_to_LibraryPage)
+        self.ui.Custom_Button.clicked.connect(self.switch_to_CustomPage)
 
         self.ui.Information_Button.clicked.connect(self.switch_to_InformationPage)
         self.ui.Settings_Button.clicked.connect(self.switch_to_SettingsPage)
-
         self.ui.Application_Button.clicked.connect(self.switch_to_ApplicationsPage)
         self.ui.Gestures_Button.clicked.connect(self.switch_to_GesturesPage)
-
         self.ui.AutoMode_Button.clicked.connect(self.switch_to_AutoModePage)
         self.ui.ManualMode_Button.clicked.connect(self.switch_to_ManualModePage)
 
@@ -80,13 +78,25 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.ui.Start_Button.clicked.connect(self.switch_to_Start_Application)
         self.ui.Stop_Button.clicked.connect(self.switch_to_Stop_Application)
 
+        self.button_group = QButtonGroup(self)
+        self.button_group.addButton(self.ui.Information_Button)
+        self.button_group.addButton(self.ui.Settings_Button)
+        self.button_group.setExclusive(True)
+
+        # Connect minimize to tray button
+        self.ui.Minimize_To_Tray_Button.clicked.connect(self.open_min_window)
+        self.min_window = None
+
+        QSizeGrip(self.ui.frame_4)
+
     def restore_or_maximize_window(self):
         if self.isMaximized():
             self.showNormal()
         else:
             self.showMaximized()
+
     def initialize_ui(self):
-        self.resize(710, 540)
+        self.resize(800, 540)
         self.setWindowTitle("MotionPilot")
 
     def switch_to_StaticGestures(self):
@@ -96,15 +106,19 @@ class MainWindow(QMainWindow,Ui_MainWindow):
     def switch_to_DynamicGestures(self):
         self.ui.Static_Dynamic_Gesture__Stacked_Widget.setCurrentIndex(1)
         self.ui.Dynamic_Button2.setChecked(True)
-    def Remove_Button_Check(self):
 
+    def Remove_Button_Check(self):
         self.ui.More_Menu_Widget.setHidden(True)
+        self.button_group.setExclusive(False)
         self.ui.Information_Button.setChecked(False)
         self.ui.Settings_Button.setChecked(False)
+        self.button_group.setExclusive(True)
+        self.ui.Main_Body_Widget.setFocus()
 
     def More_Menu_Remover(self):
         self.Remove_Button_Check()
         self.initialize_ui()
+
     def switch_to_Start_Application(self):
         self.ui.Start_Stacked_Widget.setCurrentIndex(1)
 
@@ -117,6 +131,7 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.ui.Manual_Button_Container.setHidden(True)
         self.ui.Library_Button_Container.setHidden(True)
         self.ui.Manual_Button.setChecked(False)
+        self.ui.Custom_Button.setChecked(False)
 
     def switch_to_LibraryPage(self):
         self.ui.Main_Body_Page_Stack.setCurrentIndex(2)
@@ -125,12 +140,20 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.ui.Gestures_Button.setChecked(False)
         self.ui.Application_Button.setChecked(True)
         self.ui.Manual_Button.setChecked(False)
+        self.ui.Custom_Button.setChecked(False)
 
     def switch_to_ManualPage(self):
         self.ui.Main_Body_Page_Stack.setCurrentIndex(1)
         self.ui.Manual_Stacked_Widget.setCurrentIndex(1)
         self.ui.ManualMode_Button.setChecked(False)
         self.ui.AutoMode_Button.setChecked(True)
+        self.ui.Custom_Button.setChecked(False)
+
+    def switch_to_CustomPage(self):
+        self.ui.Manual_Button.setChecked(False)
+        self.ui.Manual_Button_Container.setHidden(True)
+        self.ui.Manual_Button.setChecked(False)
+        self.ui.Library_Button_Container.setHidden(True)
 
     def switch_to_InformationPage(self):
         self.ui.More_Menu_Stacked_Widget.setCurrentIndex(0)
@@ -141,7 +164,6 @@ class MainWindow(QMainWindow,Ui_MainWindow):
     def switch_to_ApplicationsPage(self):
         self.ui.Library_Stacked_Widget.setCurrentIndex(1)
         self.ui.Applications_Stacked_Widget.setCurrentIndex(0)
-
 
     def switch_to_GesturesPage(self):
         self.ui.Library_Stacked_Widget.setCurrentIndex(0)
@@ -172,13 +194,10 @@ class MainWindow(QMainWindow,Ui_MainWindow):
     def switch_to_Page6(self):
         self.ui.Applications_Stacked_Widget.setCurrentIndex(6)
 
-
-
     def show_center_menu(self):
         self.ui.More_Menu_Widget.setHidden(False)
 
     def show_Library_Button_Container(self):
-        # self.ui.verticalSpacer_7.setHidden(False)
         self.ui.Library_Button_Container.setHidden(False)
         self.ui.Manual_Button_Container.setHidden(True)
 
@@ -186,13 +205,36 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.ui.Manual_Button_Container.setHidden(False)
         self.ui.Library_Button_Container.setHidden(True)
 
+    # Code to make the window draggable
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragPosition = event.globalPosition().toPoint() - self.frameGeometry().topLeft()
+            event.accept()
 
-        # Show window
-        self.show()
+    def mouseMoveEvent(self, event):
+        if event.buttons() & Qt.MouseButton.LeftButton:
+            if self.dragPosition is not None:
+                self.move(event.globalPosition().toPoint() - self.dragPosition)
+                event.accept()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragPosition = None
+            event.accept()
+
+    # Code to connect another window
+    def open_min_window(self):
+        from Min_tray import MinimizeWindow
+        if self.min_window is None:
+            self.min_window = MinimizeWindow()
+
+        self.min_window.show_page_one()
+        self.min_window.show()
+        self.close()
 
 # Execute App
-if __name__ =="__main__":
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window=MainWindow()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec())
